b0c70d410af1f63cde9b5f8aca30ab5daf92ac88
apoorvanand <apoorwanand@gmail.com>
2018-06-12 18:16:48 +0530

    added env

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const Hapi  = require('hapi');
 const good = require('good');
-
+var port1= process.env.PORT;
 // create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: process.env.PORT,
+    port: port1,
     routes: { cors: true } 
 });
 
