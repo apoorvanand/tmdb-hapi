3b3129938c41c981420992dacc8dd09b45c1b251
apoorvanand <apoorwanand@gmail.com>
2018-06-14 04:57:35 +0530

    movie database api

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const Hapi  = require('hapi');
 const good = require('good');
+var unirest = require("unirest");
 var port1= ~~process.env.PORT;
-music =  require('musicmatch')({apikey:"28ba71776f7f4615bf9a6c9504aaceda"});
+
 
 // create a server with a host and port
 const server = new Hapi.Server();
@@ -21,18 +22,136 @@ server.route({
             additionalHeaders: ['cache-control', 'x-requested-with']
         }},
     handler: (request, reply) => {
-        music.chartArtists({page:1, page_size:90, country:"us"})
-    .then(function(data){
-     return reply(data)
-    })
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
 
-
-
-    }
-
-
+req.query({
+  "api_key": "f25cbbe44a5bb2e9f210c7a37901a845",
+  "language": "en-US",
+  "page": "1"
 });
 
+req.headers({
+  "Postman-Token": "6f5e7285-867e-4942-9ff4-a93af40404aa",
+  "Cache-Control": "no-cache"
+});
+
+
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
+
+  console.log(res.body);
+  return reply(res.body);
+
+});
+}});
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/nowplaying',
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }},
+    handler: (request, reply) => {
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/now_playing");
+
+        req.query({
+          "page": "1",
+          "language": "en-US",
+          "api_key": "f25cbbe44a5bb2e9f210c7a37901a845"
+        });
+        
+          req.send("{}");
+        
+        req.end(function (res) {
+          if (res.error) throw new Error(res.error);
+        
+          console.log(res.body);
+          return reply(res.body);
+        });
+}});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/new',
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }},
+    handler: (request, reply) => {
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/latest");
+
+        req.query({
+          "language": "en-US",
+          "api_key": "f25cbbe44a5bb2e9f210c7a37901a845"
+        });
+        
+          req.send("{}");
+        
+        req.end(function (res) {
+          if (res.error) throw new Error(res.error);
+        
+          console.log(res.body);
+          return reply(res.body)
+        });
+}});
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/upcoming',
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }},
+    handler: (request, reply) => {
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/upcoming");
+
+req.query({
+  "page": "1",
+  "language": "en-US",
+  "api_key": "f25cbbe44a5bb2e9f210c7a37901a845"
+});
+
+  req.send("{}");
+
+req.end(function (res) {
+  if (res.error) throw new Error(res.error);
+
+  console.log(res.body);
+  return reply(res.body);
+});
+}});
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/toprated',
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }},
+    handler: (request, reply) => {
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+
+        req.query({
+          "page": "1",
+          "language": "en-US",
+          "api_key": "f25cbbe44a5bb2e9f210c7a37901a845"
+        });
+        
+          req.send("{}");
+        
+        req.end(function (res) {
+          if (res.error) throw new Error(res.error);
+        
+          console.log(res.body);
+  return reply(res.body);
+});
+}});
 // set up logging
 const options = {
     ops: {
